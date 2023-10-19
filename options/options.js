4801a16ee04d72fbcc3b65c8b38423efbae0b225
Crowdbotics <team@crowdbotics.com>
2023-10-19 04:35:39 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "ekgproductionllc-44029",
+  url: "https://ekgproductionllc-44029.botics.co",
+  api: "https://ekgproductionllc-44029.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
